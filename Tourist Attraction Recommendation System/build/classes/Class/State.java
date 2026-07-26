fbaf26aa2da49ac3805d1d40d808409d050b1ba9
Confilcted <94037746+Confilcted@users.noreply.github.com>
2026-07-27 01:23:08 +0800

    Update State.java

--- Tourist Attraction Recommendation System/build/classes/Class/State.java
+++ Tourist Attraction Recommendation System/build/classes/Class/State.java
@@ -1,1 +1,24 @@
+public enum State {
 
+    PLACEHOLDER("Placeholder State", new City[0]);
+ 
+    private final String name;
+    private City[] city;
+ 
+    State(String name, City[] city) {
+        this.name = name;
+        this.city = city;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public City[] getCity() {
+        return city;
+    }
+ 
+    public void setCity(City[] city) {
+        this.city = city;
+    }
+}
