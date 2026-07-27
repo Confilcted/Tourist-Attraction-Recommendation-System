ac18b2e5cd2ea0daaf46c69dbe45a15fcbf5c577
Confilcted <94037746+Confilcted@users.noreply.github.com>
2026-07-27 16:09:04 +0800

    Add files via upload

---
+++ Tourist Attraction Recommendation System/build/classes/Class/SearchHistory.java
@@ -0,0 +1,26 @@
+public class SearchHistory {
+
+    private Member member;
+    private State[] states;
+
+    public SearchHistory(Member member, State[] states) {
+        this.member = member;
+        this.states = states;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public State[] getStates() {
+        return states;
+    }
+
+    public void setStates(State[] states) {
+        this.states = states;
+    }
+}
