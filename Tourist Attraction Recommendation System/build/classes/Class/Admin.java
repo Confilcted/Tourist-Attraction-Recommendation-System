d76903b3f309e6e76a88bd897fb2e21879c9d9b7
Confilcted <94037746+Confilcted@users.noreply.github.com>
2026-07-27 16:07:36 +0800

    Update Admin.java

--- Tourist Attraction Recommendation System/build/classes/Class/Admin.java
+++ Tourist Attraction Recommendation System/build/classes/Class/Admin.java
@@ -2,4 +2,8 @@ public class Admin extends User {
     public Admin(String username, String password) {
         super(username, password);
     }
+
+    @Override
+    public String getRole() {
+        return "Admin";
 }
